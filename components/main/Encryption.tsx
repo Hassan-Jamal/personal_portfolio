31acbcbb926251b92cc0f0311b3a1751a0e08992
Hassan-Jamal <hassanjamal6577987@gmail.com>
2024-08-09 12:24:26 +0500

    Encryption Section Loading...

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -2,9 +2,21 @@
 
 import React from 'react'
 import motion from 'framer-motion'
+import { slideInFromTop } from '@/utils/motion'
 const Encryption = () => {
   return (
-    <div className='flex flex-row relative items-center' >
+    <div className='flex flex-row relative items-center justify-center w-full h-full' >
+        <div className='absolute w-auto h-auto top-0 z-[5]' >
+            <motion.div
+            variants={slideInFromTop}
+            className='text-[40px] font-medium text-center text-gray-200'
+            >
+                Performance
+                <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500' >
+                    
+                </span>
+            </motion.div>
+        </div>
       
     </div>
   )
