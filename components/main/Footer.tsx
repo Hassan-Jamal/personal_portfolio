a127c30c72648eca03f7fd1032f99ae2f16ca1d2
Hassan-Jamal <hassanjamal6577987@gmail.com>
2024-08-09 13:56:45 +0500

    react-icons package install,Footer Section Started

---
+++ components/main/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    RxRedditLogo,
+    RxGithubLogo,
+    RxFacebookLogo,
+    RxLinkedinLogo,
+  } from "react-icons/rx";
+  
+  import { FaYoutube } from "react-icons/fa";
+const Footer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Footer
